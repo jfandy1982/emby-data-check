79cec8c16c5efcc0fcacb8973beaad2be4d50bae
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-02-14 22:12:02 +0100

    init: Add API app using NestJs approach

---
+++ apps/emby-data-check-api/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'emby-data-check-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/emby-data-check-api',
+};
