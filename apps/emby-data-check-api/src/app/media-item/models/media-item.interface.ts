8d25c66cefeec3f6783b435f622d6a1318c515ce
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-02-18 12:32:05 +0100

    init: Add interface definitions for NestJs API

---
+++ apps/emby-data-check-api/src/app/media-item/models/media-item.interface.ts
@@ -0,0 +1,40 @@
+import { ApiProperty, PartialType, PickType } from '@nestjs/swagger';
+import { WatchStateDto } from '../../watch-state/models/watch-state.interface';
+
+export interface MediaItemDto {
+  id: string;
+  type: string;
+  providerType: string;
+  providerId: string;
+  displayName: string;
+  itemNameSlug: string;
+  path: string;
+  customTags: string;
+  watchStates?: WatchStateDto[];
+}
+
+export class MediaItemCreateDto {
+  @ApiProperty()
+  type: string;
+  @ApiProperty()
+  providerType: string;
+  @ApiProperty()
+  providerId: string;
+  @ApiProperty()
+  displayName: string;
+  @ApiProperty()
+  path: string;
+  @ApiProperty()
+  customTags: string;
+}
+
+export class MediaItemUpdateDto extends PickType(MediaItemCreateDto, [
+  'type',
+  'providerType',
+  'providerId',
+  'displayName',
+  'path',
+  'customTags',
+] as const) {}
+
+export class MediaItemFilterDto extends PartialType(MediaItemCreateDto) {}
