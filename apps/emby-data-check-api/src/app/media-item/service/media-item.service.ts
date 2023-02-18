ee80121a162430b372ee9224c0c0978812c3114c
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-02-18 12:43:42 +0100

    init: Add implementations for service classes

--- apps/emby-data-check-api/src/app/media-item/service/media-item.service.ts
+++ apps/emby-data-check-api/src/app/media-item/service/media-item.service.ts
@@ -1,4 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Observable, from, map, switchMap } from 'rxjs';
+import { Repository } from 'typeorm';
+import { MediaItemsMapper } from '../../utils/mapper/mediaItemsMapper';
+import { MediaItemEntity } from '../models/media-item.entity';
+import { MediaItemDto, MediaItemCreateDto, MediaItemUpdateDto } from '../models/media-item.interface';
 
 @Injectable()
-export class MediaItemService {}
+export class MediaItemService {
+  constructor(
+    @InjectRepository(MediaItemEntity)
+    private readonly mediaItemRepository: Repository<MediaItemEntity>
+  ) {}
+
+  findAll(): Observable<MediaItemDto[]> {
+    return from(
+      this.mediaItemRepository.find({
+        relations: ['watchStates'],
+      })
+    ).pipe(
+      map((mediaItems) => {
+        return MediaItemsMapper.mapMediaItemEntitiesToDtos(mediaItems);
+      })
+    );
+  }
+
+  findOne(id: string): Observable<MediaItemDto> {
+    return from(
+      this.mediaItemRepository.findOne({
+        relations: ['watchStates'],
+        where: { id },
+      })
+    ).pipe(
+      map((mediaItem) => {
+        if (mediaItem) {
+          return MediaItemsMapper.mapMediaItemEntityToDto(mediaItem);
+        } else {
+          throw new NotFoundException(`Media Item with ID [${id}] not found`);
+        }
+      })
+    );
+  }
+
+  createMediaItem(newMediaItem: MediaItemCreateDto): Observable<MediaItemDto> {
+    return from(this.mediaItemRepository.save(newMediaItem)).pipe(
+      map((createdMediaItem) => {
+        return MediaItemsMapper.mapMediaItemEntityToDto(createdMediaItem);
+      })
+    );
+  }
+
+  updateMediaItem(id: string, updatedMediaItem: MediaItemUpdateDto): Observable<MediaItemDto> {
+    return from(this.mediaItemRepository.update(id, updatedMediaItem)).pipe(
+      switchMap(() => {
+        return this.findOne(id);
+      })
+    );
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  deleteMediaItem(id: string): Observable<any> {
+    return from(this.mediaItemRepository.delete(id));
+  }
+}
--- apps/emby-data-check-api/src/app/user/service/user.service.ts
+++ apps/emby-data-check-api/src/app/user/service/user.service.ts
@@ -1,4 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Observable, from, map, switchMap } from 'rxjs';
+import { Repository } from 'typeorm';
+import { UsersMapper } from '../../utils/mapper/usersMapper';
+import { UserEntity } from '../models/user.entity';
+import { UserDto, UserCreateDto, UserUpdateDto } from '../models/user.interface';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>
+  ) {}
+
+  findAll(): Observable<UserDto[]> {
+    return from(
+      this.userRepository.find({
+        relations: ['embyUsers'],
+      })
+    ).pipe(
+      map((users) => {
+        return UsersMapper.mapUserEntitiesToDtos(users);
+      })
+    );
+  }
+
+  findOne(id: string): Observable<UserDto> {
+    return from(
+      this.userRepository.findOne({
+        relations: ['embyUsers'],
+        where: { id },
+      })
+    ).pipe(
+      map((user) => {
+        if (user) {
+          return UsersMapper.mapUserEntityToDto(user);
+        } else {
+          throw new NotFoundException(`User with ID [${id}] not found`);
+        }
+      })
+    );
+  }
+
+  createUser(newUser: UserCreateDto): Observable<UserDto> {
+    // TODO:
+    //  Password still saved in plain text in the database.
+    //    It will be hashed when I add an AuthModule later!!
+    return from(this.userRepository.save(newUser)).pipe(
+      map((createdUser) => {
+        return UsersMapper.mapUserEntityToDto(createdUser);
+      })
+    );
+  }
+
+  updateUser(id: string, updatedUser: UserUpdateDto): Observable<UserDto> {
+    delete updatedUser.role;
+
+    return from(this.userRepository.update(id, updatedUser)).pipe(switchMap(() => this.findOne(id)));
+  }
+
+  updateRoleOfUser(id: string, updatedUser: UserUpdateDto): Observable<UserDto> {
+    delete updatedUser.name;
+
+    return from(this.userRepository.update(id, updatedUser)).pipe(switchMap(() => this.findOne(id)));
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  deleteUser(id: string): Observable<any> {
+    return from(this.userRepository.delete(id));
+  }
+}
