6dfa1e1567a221f8986b8434d4defb595720d22f
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-02-19 14:17:15 +0100

    feat: Refactor 'findAll' to promise-based request

--- apps/emby-data-check-api/src/app/emby-user/controller/emby-user.controller.ts
+++ apps/emby-data-check-api/src/app/emby-user/controller/emby-user.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { ApiTags, ApiBearerAuth, ApiBody } from '@nestjs/swagger';
-import { Observable, map } from 'rxjs';
-import { EmbyUserDto, EmbyUserCreateDto, EmbyUserUpdateDto } from '../models/emby-user.interface';
+import { Controller, Get, HttpStatus, Query } from '@nestjs/common';
+import { ApiTags, ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
+import { Pagination } from 'nestjs-typeorm-paginate';
+import { EmbyUserDto } from '../models/emby-user.interface';
 import { EmbyUserService } from '../service/emby-user.service';
 
 @ApiTags('embyusers')
@@ -9,37 +9,40 @@ import { EmbyUserService } from '../service/emby-user.service';
 export class EmbyUserController {
   constructor(private embyUserService: EmbyUserService) {}
 
+  @ApiBearerAuth()
+  @ApiResponse({ isArray: true, status: HttpStatus.OK, description: 'List of Emby Users on Emby Server Installations' })
   @Get()
-  findEmbyUsers(): Observable<EmbyUserDto[]> {
-    return this.embyUserService.findAll();
+  async findAll(@Query('page') page = 1, @Query('limit') limit = 10): Promise<Pagination<EmbyUserDto>> {
+    limit = limit > 100 ? 100 : limit;
+    return this.embyUserService.findAll({ page, limit, route: 'http://localhost:3000/api/embyusers' });
   }
 
-  @Get(':id')
-  findOneEmbyUser(@Param('id') id: string): Observable<EmbyUserDto> {
-    return this.embyUserService.findOne(id);
-  }
+  // @Get(':id')
+  // findOneEmbyUser(@Param('id') id: string): Observable<EmbyUserDto> {
+  //   return this.embyUserService.findOne(id);
+  // }
 
-  @ApiBearerAuth()
-  @ApiBody({ type: [EmbyUserCreateDto] })
-  @Post()
-  createEmbyUser(@Body() newEmbyUser: EmbyUserCreateDto): Observable<EmbyUserDto> {
-    return this.embyUserService.createEmbyUser(newEmbyUser).pipe(
-      map((createdEmbyUser) => {
-        return createdEmbyUser;
-      })
-    );
-  }
+  // @ApiBearerAuth()
+  // @ApiBody({ type: [EmbyUserCreateDto] })
+  // @Post()
+  // createEmbyUser(@Body() newEmbyUser: EmbyUserCreateDto): Observable<EmbyUserDto> {
+  //   return this.embyUserService.createEmbyUser(newEmbyUser).pipe(
+  //     map((createdEmbyUser) => {
+  //       return createdEmbyUser;
+  //     })
+  //   );
+  // }
 
-  @ApiBearerAuth()
-  @ApiBody({ type: [EmbyUserUpdateDto] })
-  @Put(':id')
-  updateEmbyUser(@Param('id') id: string, @Body() updatedEmbyUser: EmbyUserUpdateDto): Observable<EmbyUserDto> {
-    return this.embyUserService.updateEmbyUser(id, updatedEmbyUser);
-  }
+  // @ApiBearerAuth()
+  // @ApiBody({ type: [EmbyUserUpdateDto] })
+  // @Put(':id')
+  // updateEmbyUser(@Param('id') id: string, @Body() updatedEmbyUser: EmbyUserUpdateDto): Observable<EmbyUserDto> {
+  //   return this.embyUserService.updateEmbyUser(id, updatedEmbyUser);
+  // }
 
-  @ApiBearerAuth()
-  @Delete(':id')
-  deleteEmbyUser(@Param('id') id: string): Observable<EmbyUserDto> {
-    return this.embyUserService.deleteEmbyUser(id);
-  }
+  // @ApiBearerAuth()
+  // @Delete(':id')
+  // deleteEmbyUser(@Param('id') id: string): Observable<EmbyUserDto> {
+  //   return this.embyUserService.deleteEmbyUser(id);
+  // }
 }
--- apps/emby-data-check-api/src/app/emby-user/service/emby-user.service.ts
+++ apps/emby-data-check-api/src/app/emby-user/service/emby-user.service.ts
@@ -1,10 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Observable, from, map, switchMap } from 'rxjs';
+import { IPaginationOptions, paginate, Pagination } from 'nestjs-typeorm-paginate';
 import { Repository } from 'typeorm';
-import { EmbyUsersMapper } from '../../utils/mapper/emby-usersMapper';
 import { EmbyUserEntity } from '../models/emby-user.entity';
-import { EmbyUserDto, EmbyUserCreateDto, EmbyUserUpdateDto } from '../models/emby-user.interface';
+import { EmbyUserDto } from '../models/emby-user.interface';
 
 @Injectable()
 export class EmbyUserService {
@@ -13,49 +12,41 @@ export class EmbyUserService {
     private readonly embyUserRepository: Repository<EmbyUserEntity>
   ) {}
 
-  findAll(): Observable<EmbyUserDto[]> {
-    return from(
-      this.embyUserRepository.find({
-        relations: ['user', 'installation', 'watchStates'],
-      })
-    ).pipe(
-      map((embyUsers) => {
-        return EmbyUsersMapper.mapEmbyUserEntitiesToDtos(embyUsers);
-      })
-    );
+  async findAll(options: IPaginationOptions): Promise<Pagination<EmbyUserDto>> {
+    return paginate<EmbyUserEntity>(this.embyUserRepository, options, { relations: ['user', 'installation', 'watchStates'] });
   }
 
-  findOne(id: string): Observable<EmbyUserDto> {
-    return from(
-      this.embyUserRepository.findOne({
-        relations: ['user', 'installation', 'watchStates'],
-        where: { id },
-      })
-    ).pipe(
-      map((embyUser) => {
-        if (embyUser) {
-          return EmbyUsersMapper.mapEmbyUserEntityToDto(embyUser);
-        } else {
-          throw new NotFoundException(`EmbyUser with ID [${id}] not found`);
-        }
-      })
-    );
-  }
+  // findOne(id: string): Observable<EmbyUserDto> {
+  //   return from(
+  //     this.embyUserRepository.findOne({
+  //       relations: ['user', 'installation', 'watchStates'],
+  //       where: { id },
+  //     })
+  //   ).pipe(
+  //     map((embyUser) => {
+  //       if (embyUser) {
+  //         return EmbyUsersMapper.mapEmbyUserEntityToDto(embyUser);
+  //       } else {
+  //         throw new NotFoundException(`EmbyUser with ID [${id}] not found`);
+  //       }
+  //     })
+  //   );
+  // }
 
-  createEmbyUser(newEmbyUser: EmbyUserCreateDto): Observable<EmbyUserDto> {
-    return from(this.embyUserRepository.save(newEmbyUser)).pipe(
-      map((createdEmbyUser) => {
-        return EmbyUsersMapper.mapEmbyUserEntityToDto(createdEmbyUser);
-      })
-    );
-  }
+  // createEmbyUser(newEmbyUser: EmbyUserCreateDto): Observable<EmbyUserDto> {
+  //   return from(this.embyUserRepository.save(newEmbyUser)).pipe(
+  //     map((createdEmbyUser) => {
+  //       return EmbyUsersMapper.mapEmbyUserEntityToDto(createdEmbyUser);
+  //     })
+  //   );
+  // }
 
-  updateEmbyUser(id: string, updatedEmbyUser: EmbyUserUpdateDto): Observable<EmbyUserDto> {
-    return from(this.embyUserRepository.update(id, updatedEmbyUser)).pipe(switchMap(() => this.findOne(id)));
-  }
+  // updateEmbyUser(id: string, updatedEmbyUser: EmbyUserUpdateDto): Observable<EmbyUserDto> {
+  //   return from(this.embyUserRepository.update(id, updatedEmbyUser)).pipe(switchMap(() => this.findOne(id)));
+  // }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  deleteEmbyUser(id: string): Observable<any> {
-    return from(this.embyUserRepository.delete(id));
-  }
+  // // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // deleteEmbyUser(id: string): Observable<any> {
+  //   return from(this.embyUserRepository.delete(id));
+  // }
 }
