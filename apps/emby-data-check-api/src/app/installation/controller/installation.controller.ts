5893a8a510f26d4a982f02046991ca225c729d15
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-02-19 14:11:45 +0100

    feat: Refactor 'findAll' to promise-based request

--- apps/emby-data-check-api/src/app/installation/controller/installation.controller.ts
+++ apps/emby-data-check-api/src/app/installation/controller/installation.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { ApiTags, ApiBearerAuth, ApiBody } from '@nestjs/swagger';
-import { Observable, map } from 'rxjs';
-import { InstallationDto, InstallationCreateDto, InstallationUpdateDto } from '../models/installation.interface';
+import { Controller, Get, HttpStatus, Query } from '@nestjs/common';
+import { ApiTags, ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
+import { Pagination } from 'nestjs-typeorm-paginate';
+import { InstallationDto } from '../models/installation.interface';
 import { InstallationService } from '../service/installation.service';
 
 @ApiTags('installations')
@@ -9,37 +9,40 @@ import { InstallationService } from '../service/installation.service';
 export class InstallationController {
   constructor(private installationService: InstallationService) {}
 
+  @ApiBearerAuth()
+  @ApiResponse({ isArray: true, status: HttpStatus.OK, description: 'List of Emby Server Installations' })
   @Get()
-  findInstallations(): Observable<InstallationDto[]> {
-    return this.installationService.findAll();
+  async findAll(@Query('page') page = 1, @Query('limit') limit = 10): Promise<Pagination<InstallationDto>> {
+    limit = limit > 100 ? 100 : limit;
+    return this.installationService.findAll({ page, limit, route: 'http://localhost:3000/api/installations' });
   }
 
-  @Get(':id')
-  findOneInstallation(@Param('id') id: string): Observable<InstallationDto> {
-    return this.installationService.findOne(id);
-  }
+  // @Get(':id')
+  // findOneInstallation(@Param('id') id: string): Observable<InstallationDto> {
+  //   return this.installationService.findOne(id);
+  // }
 
-  @ApiBearerAuth()
-  @ApiBody({ type: [InstallationCreateDto] })
-  @Post()
-  createInstallation(@Body() newInstallation: InstallationCreateDto): Observable<InstallationDto> {
-    return this.installationService.createInstallation(newInstallation).pipe(
-      map((createdInstallation) => {
-        return createdInstallation;
-      })
-    );
-  }
+  // @ApiBearerAuth()
+  // @ApiBody({ type: [InstallationCreateDto] })
+  // @Post()
+  // createInstallation(@Body() newInstallation: InstallationCreateDto): Observable<InstallationDto> {
+  //   return this.installationService.createInstallation(newInstallation).pipe(
+  //     map((createdInstallation) => {
+  //       return createdInstallation;
+  //     })
+  //   );
+  // }
 
-  @ApiBearerAuth()
-  @ApiBody({ type: [InstallationUpdateDto] })
-  @Put(':id')
-  updateInstallation(@Param('id') id: string, @Body() updatedInstallation: InstallationUpdateDto): Observable<InstallationDto> {
-    return this.installationService.updateInstallation(id, updatedInstallation);
-  }
+  // @ApiBearerAuth()
+  // @ApiBody({ type: [InstallationUpdateDto] })
+  // @Put(':id')
+  // updateInstallation(@Param('id') id: string, @Body() updatedInstallation: InstallationUpdateDto): Observable<InstallationDto> {
+  //   return this.installationService.updateInstallation(id, updatedInstallation);
+  // }
 
-  @ApiBearerAuth()
-  @Delete(':id')
-  deleteInstallation(@Param('id') id: string): Observable<InstallationDto> {
-    return this.installationService.deleteInstallation(id);
-  }
+  // @ApiBearerAuth()
+  // @Delete(':id')
+  // deleteInstallation(@Param('id') id: string): Observable<InstallationDto> {
+  //   return this.installationService.deleteInstallation(id);
+  // }
 }
--- apps/emby-data-check-api/src/app/installation/service/installation.service.ts
+++ apps/emby-data-check-api/src/app/installation/service/installation.service.ts
@@ -1,10 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Observable, from, map, switchMap } from 'rxjs';
+import { IPaginationOptions, paginate, Pagination } from 'nestjs-typeorm-paginate';
 import { Repository } from 'typeorm';
-import { InstallationsMapper } from '../../utils/mapper/installationsMapper';
 import { InstallationEntity } from '../models/installation.entity';
-import { InstallationDto, InstallationCreateDto, InstallationUpdateDto } from '../models/installation.interface';
+import { InstallationDto } from '../models/installation.interface';
 
 @Injectable()
 export class InstallationService {
@@ -13,53 +12,45 @@ export class InstallationService {
     private readonly installationRepository: Repository<InstallationEntity>
   ) {}
 
-  findAll(): Observable<InstallationDto[]> {
-    return from(
-      this.installationRepository.find({
-        relations: ['server', 'embyUsers'],
-      })
-    ).pipe(
-      map((installations) => {
-        return InstallationsMapper.mapInstallationEntitiesToDtos(installations);
-      })
-    );
+  async findAll(options: IPaginationOptions): Promise<Pagination<InstallationDto>> {
+    return paginate<InstallationEntity>(this.installationRepository, options, { relations: ['server', 'embyUsers'] });
   }
 
-  findOne(id: string): Observable<InstallationDto> {
-    return from(
-      this.installationRepository.findOne({
-        relations: ['server', 'embyUsers'],
-        where: { id },
-      })
-    ).pipe(
-      map((installation) => {
-        if (installation) {
-          return InstallationsMapper.mapInstallationEntityToDto(installation);
-        } else {
-          throw new NotFoundException(`Installation with ID [${id}] not found`);
-        }
-      })
-    );
-  }
+  // findOne(id: string): Observable<InstallationDto> {
+  //   return from(
+  //     this.installationRepository.findOne({
+  //       relations: ['server', 'embyUsers'],
+  //       where: { id },
+  //     })
+  //   ).pipe(
+  //     map((installation) => {
+  //       if (installation) {
+  //         return InstallationsMapper.mapInstallationEntityToDto(installation);
+  //       } else {
+  //         throw new NotFoundException(`Installation with ID [${id}] not found`);
+  //       }
+  //     })
+  //   );
+  // }
 
-  createInstallation(newInstallation: InstallationCreateDto): Observable<InstallationDto> {
-    return from(this.installationRepository.save(newInstallation)).pipe(
-      map((createdInstallation) => {
-        return InstallationsMapper.mapInstallationEntityToDto(createdInstallation);
-      })
-    );
-  }
+  // createInstallation(newInstallation: InstallationCreateDto): Observable<InstallationDto> {
+  //   return from(this.installationRepository.save(newInstallation)).pipe(
+  //     map((createdInstallation) => {
+  //       return InstallationsMapper.mapInstallationEntityToDto(createdInstallation);
+  //     })
+  //   );
+  // }
 
-  updateInstallation(id: string, updatedInstallation: InstallationUpdateDto): Observable<InstallationDto> {
-    return from(this.installationRepository.update(id, updatedInstallation)).pipe(
-      switchMap(() => {
-        return this.findOne(id);
-      })
-    );
-  }
+  // updateInstallation(id: string, updatedInstallation: InstallationUpdateDto): Observable<InstallationDto> {
+  //   return from(this.installationRepository.update(id, updatedInstallation)).pipe(
+  //     switchMap(() => {
+  //       return this.findOne(id);
+  //     })
+  //   );
+  // }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  deleteInstallation(id: string): Observable<any> {
-    return from(this.installationRepository.delete(id));
-  }
+  // // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // deleteInstallation(id: string): Observable<any> {
+  //   return from(this.installationRepository.delete(id));
+  // }
 }
