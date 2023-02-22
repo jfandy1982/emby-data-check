af23bea8516dbe0918d527d3c492a506e9938a11
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-02-22 21:46:08 +0100

    break: Remove version from global API prefix

--- apps/emby-data-check-api/src/main.ts
+++ apps/emby-data-check-api/src/main.ts
@@ -11,7 +11,7 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api/v1';
+  const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
 
