c0fc0a52cbd42c49d810f018b0180e62c448427e
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-03-04 18:27:10 +0100

    feat: Inject mock objects for tests

--- apps/emby-data-check-api/src/app/installation/service/installation-db.service.spec.ts
+++ apps/emby-data-check-api/src/app/installation/service/installation-db.service.spec.ts
@@ -1,18 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { InstallationEntity } from '../models/installation.entity';
 import { InstallationDbService } from './installation-db.service';
 
 describe('InstallationService', () => {
   let service: InstallationDbService;
 
-  const mockInstallationDbService = {};
+  const mockInstallationEntityRepository = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [InstallationDbService],
-    })
-      .overrideProvider(InstallationDbService)
-      .useValue(mockInstallationDbService)
-      .compile();
+      providers: [InstallationDbService, { provide: getRepositoryToken(InstallationEntity), useValue: mockInstallationEntityRepository }],
+    }).compile();
 
     service = module.get<InstallationDbService>(InstallationDbService);
   });
