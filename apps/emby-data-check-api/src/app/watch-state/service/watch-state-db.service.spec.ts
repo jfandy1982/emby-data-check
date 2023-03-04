ac073e2c63166e00465d0f10954902836a0e3e76
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-03-04 17:59:41 +0100

    feat: Inject mock objects for tests

--- apps/emby-data-check-api/src/app/watch-state/service/watch-state-db.service.spec.ts
+++ apps/emby-data-check-api/src/app/watch-state/service/watch-state-db.service.spec.ts
@@ -1,18 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { WatchStateEntity } from '../models/watch-state.entity';
 import { WatchStateDbService } from './watch-state-db.service';
 
 describe('WatchStateDbService', () => {
   let service: WatchStateDbService;
 
-  const mockWatchStateDbService = {};
+  const mockWatchStateEntityRepository = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WatchStateDbService],
-    })
-      .overrideProvider(WatchStateDbService)
-      .useValue(mockWatchStateDbService)
-      .compile();
+      providers: [WatchStateDbService, { provide: getRepositoryToken(WatchStateEntity), useValue: mockWatchStateEntityRepository }],
+    }).compile();
 
     service = module.get<WatchStateDbService>(WatchStateDbService);
   });
