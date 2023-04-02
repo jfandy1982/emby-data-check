9d3a40c619e3b61680072857e01597294d73fe01
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-04-02 14:15:32 +0200

    Add EmbyDataCheck Backend app (NestJs)

---
+++ apps/emby-data-check-api/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'emby-data-check-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/emby-data-check-api',
+};
