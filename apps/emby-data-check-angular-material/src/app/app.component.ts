c25cf67187adc3e76e523d606b1714705d056a9e
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-04-02 20:47:38 +0200

    feat: remove generated content from Nx

--- apps/emby-data-check-angular-material/src/app/app.component.ts
+++ apps/emby-data-check-angular-material/src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'emby-data-check-angular-material';
-}
+export class AppComponent {}
--- apps/emby-data-check-angular-material/src/app/app.module.ts
+++ apps/emby-data-check-angular-material/src/app/app.module.ts
@@ -3,10 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' })],
   providers: [],
   bootstrap: [AppComponent],
