8f5e547f63b5c169e109d0b57b0a275d00545ad9
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-04-06 19:08:21 +0200

    chore: Set up project to basic logic

--- apps/edc-ui-fundamentals/jest.config.ts
+++ apps/edc-ui-fundamentals/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
-  displayName: 'fundamentals-ui-edc2',
+  displayName: 'edc-ui-fundamentals',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/apps/fundamentals-ui-edc2',
+  coverageDirectory: '../../coverage/apps/edc-ui-fundamentals',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
---
+++ apps/edc-ui-fundamentals/src/app/app.component.spec.ts
@@ -0,0 +1,26 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent, NxWelcomeComponent],
+    }).compileComponents();
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome edc-ui-fundamentals');
+  });
+
+  it(`should have as title 'edc-ui-fundamentals'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('edc-ui-fundamentals');
+  });
+});
