ab0a9ed6aba65b1a5d228b039db3c15c15dd3c92
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-04-09 15:56:52 +0200

    feat: Enable migrations path config

--- apps/edc-api/src/app/configuration/typeorm.service.ts
+++ apps/edc-api/src/app/configuration/typeorm.service.ts
@@ -16,7 +16,7 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       database: this.configService.get<string>('database.name'),
       entities: [path.join(__dirname, '../../') + '/**/*.entity.ts'],
       host: this.configService.get<string>('database.host'),
-      //migrations: [path.join(__dirname, '../../') + 'db/migrations/*.ts'],
+      migrations: [path.join(__dirname, '../../') + 'db/migrations/*.ts'],
       password: this.configService.get<string>('database.password'),
       port: this.configService.get<number>('database.port'),
       synchronize: this.configService.get<boolean>('database.synchronize'),
