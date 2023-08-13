0ee27a8238f5f84ae452ec70369a022d64a0473d
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-08-13 11:17:57 +0200

    chore(edc-ui-fundamentals): connect fundamentals library into UI app

--- apps/edc-ui-fundamentals/src/app/app.component.ts
+++ apps/edc-ui-fundamentals/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ButtonModule } from '@fundamental-ngx/core/button';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ButtonModule],
   selector: 'edc-fundamentals-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
