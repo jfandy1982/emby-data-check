4e2a5fc12cf4f576f13834dde5d128d7f335b4a5
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-08-27 12:15:14 +0200

    feat(edc-api): add Configuration for Swagger in API app

--- apps/edc-api/src/main.ts
+++ apps/edc-api/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerDocumentOptions, SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -12,7 +13,27 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.enableCors();
   const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Emby Data Check')
+    .setDescription(
+      '<p>The Emby Data Check API is the backend for an opinionated check tool of an Emby server. Please refer to <a href="https://github.com/jfandy1982/emby-data-check">this GitHub repository</a> for more context information.</p>'
+    )
+    .setVersion('v1')
+    .addBearerAuth()
+    .addTag('Emby')
+    .build();
+  const options: SwaggerDocumentOptions = {
+    deepScanRoutes: true,
+  };
+  const customOptions: SwaggerCustomOptions = {
+    customSiteTitle: 'Emby Data Check - API Docs',
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('swagger', app, document, customOptions);
+
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
