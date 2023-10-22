94907705c315467c035f7d4d43e6ad415d59e8a8
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-10-22 22:17:08 +0200

    feat(shared-interfaces-db): set up entities in library

--- libs/shared-interfaces/db-interface/src/index.ts
+++ libs/shared-interfaces/db-interface/src/index.ts
@@ -1,1 +1,6 @@
-export * from './lib/dummy';
+export * from './lib/entities/emby-user.entity';
+export * from './lib/entities/installation.entity';
+export * from './lib/entities/media-item.entity';
+export * from './lib/entities/server.entity';
+export * from './lib/entities/user.entity';
+export * from './lib/entities/watch-state.entity';
