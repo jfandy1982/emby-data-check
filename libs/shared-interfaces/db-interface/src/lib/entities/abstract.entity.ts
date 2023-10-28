b68fa75fce3757ca3fe15dab09b0a416ad15f27f
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-10-28 14:04:05 +0200

    feat(shared-interfaces-db): add validation configuration settings

--- libs/shared-interfaces/db-interface/src/lib/entities/abstract.entity.ts
+++ libs/shared-interfaces/db-interface/src/lib/entities/abstract.entity.ts
@@ -1,12 +1,25 @@
+import { IsDate, IsString, IsUUID } from 'class-validator';
 import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 export abstract class AbstractEntity {
   @PrimaryGeneratedColumn('uuid')
+  @IsString({
+    context: { entity: 'abstract', className: 'AbstractEntity', errorCode: 'validation-0001' },
+  })
+  @IsUUID('4', {
+    context: { entity: 'abstract', className: 'AbstractEntity', errorCode: 'validation-0002' },
+  })
   id: string;
 
   @CreateDateColumn()
+  @IsDate({
+    context: { entity: 'abstract', className: 'AbstractEntity', errorCode: 'validation-0003' },
+  })
   createdAt: Date;
 
   @UpdateDateColumn()
+  @IsDate({
+    context: { entity: 'abstract', className: 'AbstractEntity', errorCode: 'validation-0004' },
+  })
   changedAt: Date;
 }
