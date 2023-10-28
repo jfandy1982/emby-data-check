78f2b34b2f55dee7ba3cf001dc0e1c3fbf80a0c8
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-10-28 14:27:10 +0200

    feat(shared-interfaces-db): add validation configuration settings

--- libs/shared-interfaces/db-interface/src/lib/entities/watch-state.entity.ts
+++ libs/shared-interfaces/db-interface/src/lib/entities/watch-state.entity.ts
@@ -1,3 +1,4 @@
+import { IsBoolean, IsDate } from 'class-validator';
 import { Column, Entity, ManyToOne } from 'typeorm';
 import { AbstractEntity } from './abstract.entity';
 import { EmbyUserEntity } from './emby-user.entity';
@@ -6,9 +7,15 @@ import { MediaItemEntity } from './media-item.entity';
 @Entity('watch-state')
 export class WatchStateEntity extends AbstractEntity {
   @Column({ type: 'boolean', default: false })
+  @IsBoolean({
+    context: { entity: 'watch-state', className: 'WatchStateEntity', errorCode: 'validation-0001' },
+  })
   isWatched: boolean;
 
   @Column({ nullable: true })
+  @IsDate({
+    context: { entity: 'watch-state', className: 'WatchStateEntity', errorCode: 'validation-0002' },
+  })
   lastWatchedAt: Date;
 
   @ManyToOne(() => EmbyUserEntity, (embyUser) => embyUser.watchStates, {
