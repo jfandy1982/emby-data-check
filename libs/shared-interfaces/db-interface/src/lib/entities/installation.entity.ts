b604a3b5a15dee806b2abf40522b8f5d84b2330c
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-10-28 14:25:11 +0200

    feat(shared-interfaces-db): add validation configuration settings

--- libs/shared-interfaces/db-interface/src/lib/entities/installation.entity.ts
+++ libs/shared-interfaces/db-interface/src/lib/entities/installation.entity.ts
@@ -1,3 +1,4 @@
+import { IsBoolean, IsString, Length } from 'class-validator';
 import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
 import { AbstractEntity } from './abstract.entity';
 import { EmbyUserEntity } from './emby-user.entity';
@@ -6,9 +7,18 @@ import { ServerEntity } from './server.entity';
 @Entity('installation')
 export class InstallationEntity extends AbstractEntity {
   @Column({ type: 'varchar', nullable: false, length: 32, unique: true })
+  @IsString({
+    context: { entity: 'installation', className: 'InstallationEntity', errorCode: 'validation-0001' },
+  })
+  @Length(32, 32, {
+    context: { entity: 'installation', className: 'InstallationEntity', errorCode: 'validation-0002' },
+  })
   serverIdFromEmbyDb: string;
 
   @Column({ type: 'boolean', default: false })
+  @IsBoolean({
+    context: { entity: 'installation', className: 'InstallationEntity', errorCode: 'validation-0003' },
+  })
   isActive: boolean;
 
   @ManyToOne(() => ServerEntity, (server) => server.installations, {
