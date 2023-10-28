65ed8de91b5278ec85a42f0622181274e40f9e33
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-10-28 13:59:38 +0200

    feat(shared-interfaces-db): add validation configuration settings

--- libs/shared-interfaces/db-interface/src/lib/entities/server.entity.ts
+++ libs/shared-interfaces/db-interface/src/lib/entities/server.entity.ts
@@ -1,3 +1,4 @@
+import { IsIP, IsInt, IsString, Length, Max, MaxLength, Min } from 'class-validator';
 import { Column, Entity, OneToMany } from 'typeorm';
 import { AbstractEntity } from './abstract.entity';
 import { InstallationEntity } from './installation.entity';
@@ -5,18 +6,51 @@ import { InstallationEntity } from './installation.entity';
 @Entity('server')
 export class ServerEntity extends AbstractEntity {
   @Column({ type: 'varchar', nullable: false, length: 50, unique: true })
+  @IsString({
+    context: { entity: 'server', className: 'ServerEntity', errorCode: 'validation-0001' },
+  })
+  @MaxLength(50, {
+    context: { entity: 'server', className: 'ServerEntity', errorCode: 'validation-0002' },
+  })
   servername: string;
 
   @Column({ type: 'varchar', nullable: true, length: 100 })
+  @IsString({
+    context: { entity: 'server', className: 'ServerEntity', errorCode: 'validation-0003' },
+  })
+  @MaxLength(100, {
+    context: { entity: 'server', className: 'ServerEntity', errorCode: 'validation-0004' },
+  })
   description: string;
 
   @Column({ type: 'varchar', nullable: true, length: 32 })
+  @IsString({
+    context: { entity: 'server', className: 'ServerEntity', errorCode: 'validation-0005' },
+  })
+  @Length(32, 32, {
+    context: { entity: 'server', className: 'ServerEntity', errorCode: 'validation-0006' },
+  })
   apiKey: string;
 
   @Column({ type: 'inet', nullable: true })
+  @IsString({
+    context: { entity: 'server', className: 'ServerEntity', errorCode: 'validation-0007' },
+  })
+  @IsIP('4', {
+    context: { entity: 'server', className: 'ServerEntity', errorCode: 'validation-0008' },
+  })
   ipAddress: string;
 
   @Column({ type: 'smallint', nullable: true })
+  @IsInt({
+    context: { entity: 'server', className: 'ServerEntity', errorCode: 'validation-0009' },
+  })
+  @Min(1024, {
+    context: { entity: 'server', className: 'ServerEntity', errorCode: 'validation-0010' },
+  })
+  @Max(65535, {
+    context: { entity: 'server', className: 'ServerEntity', errorCode: 'validation-0011' },
+  })
   port: number;
 
   @OneToMany(() => InstallationEntity, (installation) => installation.server, {
