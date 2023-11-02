311a461dbe24e28731be2bca2eb677a6c34d27fb
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-11-02 20:10:36 +0100

    feat(shared-interfaces-enums): add enums for media items

---
+++ libs/shared-interfaces/enum/src/lib/media-type.enum.ts
@@ -0,0 +1,9 @@
+/* Keep in sync with class MediaItemEntity in lib '@edc/shared-interfaces/db-interface' */
+export enum MediaItemType {
+  EPISODE = 'Episode',
+  MOVIE = 'Movie',
+  MUSICVIDEO = 'MusicVideo',
+  SEASON = 'Season',
+  SERIES = 'Series',
+  VIDEO = 'Video',
+}
