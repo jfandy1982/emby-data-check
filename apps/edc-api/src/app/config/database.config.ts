baf3c2a992a43fe411d45d1acd4d22c38766658d
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-11-04 12:47:22 +0100

    refactor(shared-interfaces-db): hard-code table name for migrations table

--- apps/edc-api/src/app/config/database.config.ts
+++ apps/edc-api/src/app/config/database.config.ts
@@ -2,15 +2,10 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
   type: 'postgres',
-  schema: process.env.DATABASE_SCHEMA || 'postgres',
+  database: process.env.DATABASE_NAME || 'postgres',
+  schema: process.env.DATABASE_SCHEMA || 'public',
   host: process.env.DATABASE_HOST || 'localhost',
   port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  entities: [`${__dirname}/../../../../../libs/shared-interfaces/db-interface/src/**/*.entity{.ts,.js}`],
-  synchronize: process.env.NODE_ENV === 'development',
-  logging: process.env.NODE_ENV === 'development',
-  migrations: [`${__dirname}/../../../db/migrations/*{.ts,.js}`],
-  migrationsTableName: process.env.MIGRATIONS_TABLE_NAME,
 }));
