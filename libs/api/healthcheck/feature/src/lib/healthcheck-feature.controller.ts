26fadc314d214b1ced6217edaf5b521126cb08ac
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2023-11-28 21:42:01 +0100

    feat(healthcheck-feature): add api config for swagger

--- libs/api/healthcheck/feature/src/lib/healthcheck-feature.controller.ts
+++ libs/api/healthcheck/feature/src/lib/healthcheck-feature.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { HealthCheck, HealthCheckService, TypeOrmHealthIndicator } from '@nestjs/terminus';
 
-@Controller('healthcheck-feature')
+@ApiTags('health')
+@Controller({ path: 'health' })
 export class HealthcheckFeatureController {
   constructor(
     private health: HealthCheckService,
