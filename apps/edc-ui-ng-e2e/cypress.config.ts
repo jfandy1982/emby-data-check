8e5636283cea613df2dd9e1ecedce786d29c92c6
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2024-02-03 15:29:37 +0100

    chore(repo): install new app 'edc-ui-ng' as ui project

---
+++ apps/edc-ui-ng-e2e/cypress.config.ts
@@ -0,0 +1,14 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: { default: 'nx run edc-ui-ng:serve:development', production: 'nx run edc-ui-ng:serve:production' },
+      ciWebServerCommand: 'nx run edc-ui-ng:serve-static',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
