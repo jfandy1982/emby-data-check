fa38a911f75f54d0d49ce0dbc2fee728488065b0
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2024-02-25 18:50:24 +0100

    feat(edc-api): restructure data-access libs for user feature

--- libs/api/user/data-access/edc/jest.config.ts
+++ libs/api/user/data-access/edc/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'user-emby-data-access',
-  preset: '../../../../jest.preset.js',
+  displayName: 'user-data-access-edc',
+  preset: '../../../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../../coverage/libs/api/user-emby/data-access',
+  coverageDirectory: '../../../../../coverage/libs/api/user-edc/data-access',
 };
--- libs/api/user/data-access/emby/jest.config.ts
+++ libs/api/user/data-access/emby/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'user-edc-data-access',
-  preset: '../../../../jest.preset.js',
+  displayName: 'user-data-access-emby',
+  preset: '../../../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../../coverage/libs/api/user-edc/data-access',
+  coverageDirectory: '../../../../../coverage/libs/api/user/data-access/emby',
 };
