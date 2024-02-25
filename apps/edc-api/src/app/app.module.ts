52cabeaa9c0dfb415e8413de7a8130a9b85d6b1b
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2024-02-25 20:10:41 +0100

    feat(edc-api): add imports for mediaitem feature and user feature

--- apps/edc-api/src/app/app.module.ts
+++ apps/edc-api/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { AppConfig, DatabaseConfig } from './config';
 import { HealthcheckFeatureModule } from '@edc/api/healthcheck/feature';
+import { MediaitemFeatureModule } from '@edc/api/mediaitem/feature';
 import { ServerFeatureModule } from '@edc/api/server/feature';
+import { UserFeatureModule } from '@edc/api/user/feature';
+import { AppConfig, DatabaseConfig } from './config';
 
 @Module({
   imports: [
@@ -22,7 +23,9 @@ import { ServerFeatureModule } from '@edc/api/server/feature';
       inject: [ConfigService],
     }),
     HealthcheckFeatureModule,
+    MediaitemFeatureModule,
     ServerFeatureModule,
+    UserFeatureModule,
   ],
   controllers: [],
   providers: [],
