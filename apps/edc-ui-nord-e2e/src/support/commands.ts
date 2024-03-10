065169c71b41615bf5b6be47097830c72bf3991d
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2024-03-10 13:44:53 +0100

    chore: mark unused imports as error while linting

--- apps/edc-ui-nord-e2e/src/support/commands.ts
+++ apps/edc-ui-nord-e2e/src/support/commands.ts
@@ -12,15 +12,13 @@
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 declare namespace Cypress {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface Chainable<Subject> {
     login(email: string, password: string): void;
   }
 }
 
 // -- This is a parent command --
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-Cypress.Commands.add('login', (email, password) => {
+Cypress.Commands.add('login', (_email, _password) => {
   //console.log('Custom command example: Login', email, password);
 });
 //
