bb3bd7057533a405cbb3c5dadffeb37f716c4c89
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2024-03-21 20:34:27 +0100

    feat: add header and footer UI libs

--- apps/edc-ui-fundamentals/src/app/app.component.ts
+++ apps/edc-ui-fundamentals/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from '@edc/ui/footer/ngx';
+import { HeaderComponent } from '@edc/ui/header/ngx';
 import { ButtonComponent } from '@fundamental-ngx/core/button';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, ButtonComponent],
+  imports: [RouterModule, ButtonComponent, HeaderComponent, FooterComponent],
   selector: 'edc-ngx-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- apps/edc-ui-ng/src/app/app.component.ts
+++ apps/edc-ui-ng/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from '@edc/ui/footer/ng';
+import { HeaderComponent } from '@edc/ui/header/ng';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, MatButtonModule],
+  imports: [RouterModule, MatButtonModule, HeaderComponent, FooterComponent],
   selector: 'edc-ng-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
