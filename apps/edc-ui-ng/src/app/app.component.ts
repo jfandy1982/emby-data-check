04b22f8c9b11389a831fc255b11407a67a166ffe
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2024-03-23 16:51:49 +0100

    feat (edc-ui-ng): remove unused import

--- apps/edc-ui-ng/src/app/app.component.ts
+++ apps/edc-ui-ng/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from '@edc/ui/footer/ng';
 import { HeaderComponent } from '@edc/ui/header/ng';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, MatButtonModule, HeaderComponent, FooterComponent],
+  imports: [RouterModule, HeaderComponent, FooterComponent],
   selector: 'edc-ng-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
