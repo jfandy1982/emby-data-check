6b374a6f241cff3f4d816f67dcf5d2890d6ebb5f
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2025-03-01 09:40:16 +0100

    feat: :label: add libraries for type definitions

---
+++ libs/shared-interfaces/db-interface/src/index.ts
@@ -0,0 +1,7 @@
+export * from './lib/entities/item.entity';
+export * from './lib/entities/iteminstance.entity';
+export * from './lib/entities/server.entity';
+export * from './lib/entities/serverinstance.entity';
+export * from './lib/entities/user.entity';
+export * from './lib/entities/userinstance.entity';
+export * from './lib/entities/watchedinstance.entity';
