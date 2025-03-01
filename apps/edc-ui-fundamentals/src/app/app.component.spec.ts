468b4ac461a629a2dabfe5bd22529b751cfbf2c1
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2025-03-01 17:34:56 +0100

    fix: fix linting errors

--- apps/edc-ui-fundamentals/src/app/app.component.spec.ts
+++ apps/edc-ui-fundamentals/src/app/app.component.spec.ts
@@ -1,12 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterModule.forRoot([])],
+      imports: [AppComponent, RouterModule.forRoot([])],
     }).compileComponents();
   });
 
