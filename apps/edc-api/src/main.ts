e2abbeef262c5db539e666e38d6ffb9ae1c9e481
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2025-03-02 12:18:09 +0100

    feat: :technologist: setup swagger for APIs with basic setting

--- apps/edc-api/src/main.ts
+++ apps/edc-api/src/main.ts
@@ -4,16 +4,35 @@
  */
 
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { environment } from '@edc/shared/environment';
+import { setupSwagger } from './util';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //const configService = app.get(ConfigService);
+  //const port = configService.get('config.port');
+  const port = 3000;
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+
+  // TODO - revisit and secure this!
+  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+  environment.production === true
+    ? app.enableCors()
+    : app.enableCors({
+        origin: ['http://localhost:4200', 'http://localhost:4201', 'http://localhost:4202', 'http://localhost:4203'],
+        credentials: true,
+      });
+
+  setupSwagger(app, globalPrefix);
+
+  await app.listen(port, () => {
+    Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  });
 }
 
 bootstrap();
--- libs/api/healthcheck/feature/src/lib/healthcheck-feature.module.ts
+++ libs/api/healthcheck/feature/src/lib/healthcheck-feature.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
 import { HealthcheckFeatureController } from './healthcheck-feature.controller';
 
 @Module({
+  imports: [TerminusModule],
   controllers: [HealthcheckFeatureController],
   providers: [],
   exports: [],
