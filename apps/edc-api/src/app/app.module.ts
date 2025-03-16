fce5ed64082f34d65b74a86b0570d280dd8da6df
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2025-03-16 14:47:53 +0100

    feat: :safety_vest: add validation for environment variables

--- apps/edc-api/src/app/app.module.ts
+++ apps/edc-api/src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { HealthcheckFeatureModule } from '@edc/api/healthcheck/feature';
+import * as joi from 'joi';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { HealthcheckFeatureModule } from '@edc/api/healthcheck/feature';
 import { ApiConfig, DbConfig } from './configEnvVars';
 @Module({
   imports: [
@@ -9,6 +10,15 @@ import { ApiConfig, DbConfig } from './configEnvVars';
       cache: true,
       expandVariables: true,
       load: [ApiConfig, DbConfig],
+      validationSchema: joi.object({
+        API_PORT: joi.number().min(1025).max(65535).default(3000),
+        NODE_ENV: joi.string().valid('ci', 'development', 'production', 'test').default('development'),
+      }),
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
+      validatePredefined: true,
     }),
     HealthcheckFeatureModule,
   ],
