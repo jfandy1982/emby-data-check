056157e4763c9d77be0299e6c892da9c365dc385
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2025-03-16 17:38:46 +0100

    feat: :sparkles: add endpoint to fetch PublicSystemInfo from configured Emby Server

--- libs/api/server/data-access/edc/src/index.ts
+++ libs/api/server/data-access/edc/src/index.ts
@@ -1,2 +1,1 @@
-export * from './lib/server-data-access-edc.service';
 export * from './lib/server-data-access-edc.module';
--- libs/api/server/feature/src/index.ts
+++ libs/api/server/feature/src/index.ts
@@ -1,3 +1,1 @@
-export * from './lib/server-feature.controller';
-export * from './lib/server-feature.service';
 export * from './lib/server-feature.module';
