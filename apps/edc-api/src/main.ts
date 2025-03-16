34186cb3c5c3512042c50001b41a12715b2505aa
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2025-03-16 14:48:54 +0100

    feat: :safety_vest: enable validation pipe for requests globally

--- apps/edc-api/src/main.ts
+++ apps/edc-api/src/main.ts
@@ -3,7 +3,7 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { environment, IApiConfigEnvVars, IEnvironment } from '@edc/shared/environment';
@@ -29,6 +29,13 @@ async function bootstrap() {
       });
 
   setupSwagger(app, globalPrefix);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(port, () => {
     Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
