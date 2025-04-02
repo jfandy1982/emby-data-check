86e4f5a9f0e704a2988234a01784f65389b1fab9
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2025-04-02 21:08:44 +0200

    feat: :construction: connect Ng UI app with edc-api endpoint

--- libs/api/server/feature/src/lib/server-feature.controller.ts
+++ libs/api/server/feature/src/lib/server-feature.controller.ts
@@ -1,18 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { ServerFeatureService } from './server-feature.service';
 import { environment } from '@edc/shared/environment';
+import { EmbyPublicSystemInfoInterface } from '@edc/shared/model/emby';
+import { Observable, of } from 'rxjs';
 
 @Controller('servers')
 export class ServerFeatureController {
   constructor(private readonly serverFeatureService: ServerFeatureService) {}
 
   @Get('publicSystemInfo')
-  getPublicSystemInfo() {
-    this.serverFeatureService
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      .getPublicSystemInfo(environment.embyConfigEnvVars!.emby_server_host, environment.embyConfigEnvVars!.emby_server_port)
-      .subscribe((data) => {
-        return data;
-      });
+  getPublicSystemInfo(): Observable<EmbyPublicSystemInfoInterface> {
+    return (
+      this.serverFeatureService
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        .getPublicSystemInfo(environment.embyConfigEnvVars!.emby_server_host, environment.embyConfigEnvVars!.emby_server_port)
+    );
   }
 }
