12284ccb9641568e17bdfa8bdacbcd6783493cea
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2025-05-05 20:53:13 +0200

    fix: :white_check_mark: fix failing tests

--- libs/api/server/data-access/emby/src/lib/server-data-access-emby.service.spec.ts
+++ libs/api/server/data-access/emby/src/lib/server-data-access-emby.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test } from '@nestjs/testing';
 import { ServerDataAccessEmbyService } from './server-data-access-emby.service';
+import { HttpModule } from '@nestjs/axios';
 
 describe('ServerDataAccessEmbyService', () => {
   let service: ServerDataAccessEmbyService;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
+      imports: [HttpModule],
       providers: [ServerDataAccessEmbyService],
     }).compile();
 
