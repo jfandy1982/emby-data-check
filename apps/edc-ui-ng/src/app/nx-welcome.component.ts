a18a0edec2e1cdbcfc394df88ecc87aeefe3b786
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2025-05-17 12:19:20 +0200

    chore: :tada: restart with new nx workspace version

---
+++ apps/edc-ui-ng/src/app/nx-welcome.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'edc-ng-nx-welcome',
+  imports: [CommonModule],
+  template: ``,
+  styles: [],
+  encapsulation: ViewEncapsulation.None,
+})
+export class NxWelcomeComponent {}
---
+++ sheriff.config.ts
@@ -0,0 +1,19 @@
+import { SheriffConfig } from '@softarc/sheriff-core';
+
+/**
+ * Minimal configuration for Sheriff
+ * Assigns the 'noTag' tag to all modules and
+ * allows all modules to depend on each other.
+ */
+
+export const config: SheriffConfig = {
+  modules: {}, // apply tags to your modules
+  depRules: {
+    // root is a virtual module, which contains all files not being part
+    // of any module, e.g. application shell, main.ts, etc.
+    root: 'noTag',
+    noTag: 'noTag',
+
+    // add your dependency rules here
+  },
+};
