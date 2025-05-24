522f8bb86f575aee72b9f6b34120309f2b094072
Andreas Ziegler <12111541+jfandy1982@users.noreply.github.com>
2025-05-24 15:11:17 +0200

    feat: :construction: enable shutdown hooks for API

--- apps/edc-api/src/main.ts
+++ apps/edc-api/src/main.ts
@@ -9,9 +9,13 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+
+  app.enableShutdownHooks();
+
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
